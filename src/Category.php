cde2a7bb56f558dfde36f70105f07c6c533245e0
Jenna Schold Navratil & Jordan Navratil <student@epicodus.com>
2015-09-01 16:30:37 -0700

    add completed category tests and category src file.

--- src/Category.php
+++ src/Category.php
@@ -32,12 +32,43 @@
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
+        function update()
+        {
+            $GLOBALS['DB']->exec("UPDATE categories SET category_name = '{$this->getCategoryName()}' WHERE id = {$this->getId()};");
+        }
+
+        function deleteOne()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM categories WHERE id = {$this->getId()};");
+        }
+
         static function deleteAll()
         {
             $GLOBALS['DB']->exec("DELETE FROM categories;");
         }
 
+        static function getAll()
+        {
+            $categories_returned = $GLOBALS['DB']->query("SELECT * FROM categories;");
+            $categories = array();
+            foreach($categories_returned as $category){
+                $id = $category['id'];
+                $category_name = $category['category_name'];
+                $new_category = new Category($category_name, $id);
+                array_push($categories, $new_category);
+            }
+            return $categories;
+        }
 
+        static function find($searchId)
+        {
+            $categories_returned = Category::getAll();
+            foreach($categories_returned as $category){
+               if ($searchId == $category->getId()){
+                   return $category;
+               }
+            }
+        }
 
     }
 
--- tests/CategoryTest.php
+++ tests/CategoryTest.php
@@ -44,23 +44,101 @@
         {
             $category_name = "music";
             $id = 1;
-            $new_category = new Category($category_name);
+            $new_category = new Category($category_name, $id);
 
             $result = $new_category->getId();
 
             $this->assertEquals(1, $result);
         }
 
+        function testGetAll()
+        {
+            $category_name = "music";
+            $new_category = new Category($category_name);
+            $new_category->save();
+
+            $category_name2 = "arts";
+            $new_category2 = new Category($category_name);
+            $new_category2->save();
+
+            $result = Category::getAll();
+
+            $this->assertEquals([$new_category, $new_category2], $result);
+        }
+
         function testSave()
         {
+            $category_name = "music";
+            $new_category = new Category($category_name);
 
+            $new_category->save();
+            $result = Category::getAll();
+
+            $this->assertEquals($new_category, $result[0]);
         }
 
         function testDeleteAll()
         {
+            $category_name = "music";
+            $new_category = new Category($category_name);
+            $new_category->save();
+
+            $category_name2 = "arts";
+            $new_category2 = new Category($category_name);
+            $new_category2->save();
+
+            Category::deleteAll();
+            $result = Category::GetAll();
+
+            $this->assertEquals([], $result);
+        }
+
+        function testDeleteOne()
+        {
+            $category_name = "music";
+            $new_category = new Category($category_name);
+            $new_category->save();
+
+            $category_name2 = "arts";
+            $new_category2 = new Category($category_name);
+            $new_category2->save();
+
+            $new_category->deleteOne();
+
+            $result = Category::getAll();
+
+            $this->assertEquals($new_category2, $result[0]);
 
         }
 
+        function testUpdate()
+        {
+            $category_name = "music";
+            $new_category = new Category($category_name);
+            $new_category->save();
+            $new_category->setCategoryName("concerts");
+            $new_category->update();
+
+            $result = Category::getAll();
+
+            $this->assertEquals($new_category, $result[0]);
+        }
+
+        function testFind()
+        {
+            $category_name = "music";
+            $new_category = new Category($category_name);
+            $new_category->save();
+
+            $category_name2 = "arts";
+            $new_category2 = new Category($category_name);
+            $new_category2->save();
+
+            $result = Category::find($new_category->getId());
+
+            $this->assertEquals($new_category, $result);
+
+        }
 
     }
 ?>
